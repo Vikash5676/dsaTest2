3e49903aca1ee84793f403aa93b937b6cdf9c8a8
vikash kumar <vikashkumar04081999@gmail.com>
2022-08-01 13:28:49 +0530

    modified q6

--- q6.js
+++ q6.js
@@ -22,13 +22,37 @@ class LL {
     }
   }
   findLength() {
-    let count = 0;
     let curr = this.head;
+    let count = 0;
     while (curr != null) {
-      count += 1;
+      count++;
       curr = curr.next;
     }
     return count;
+  }
+  rev() {
+    let arr = [];
+    let curr = this.head;
+    while (curr != null) {
+      arr.push(curr.data);
+      curr = curr.next;
+    }
+    arr.reverse();
+    return arr;
+  }
+
+  checkPalindrome(rev, c) {
+    let curr = this.head;
+    let count = 0;
+
+    for (let i = 0; i < rev.length; i++) {
+      if (rev[i] == curr.data) {
+        count++;
+        curr = curr.next;
+      }
+    }
+    if (count == c) console.log("the given ll is palindrome");
+    else console.log("the given ll is not a palindrome");
   }
   printLL() {
     var temp = this.head;
@@ -43,11 +67,12 @@ var ll = new LL();
 ll.takeInput(2);
 ll.takeInput(3);
 ll.takeInput(4);
-ll.takeInput(5);
-ll.takeInput(6);
+ll.takeInput(3);
+ll.takeInput(2);
 // ll.takeInput(7);
 //ll.printLL();
 // ll.deleteEle(3);
-let mid = ll.findMiddle();
-ll.reverse(mid);
+let c = ll.findLength();
+let ll1 = ll.rev();
+ll.checkPalindrome(ll1, c);
 // ll.printLL();
